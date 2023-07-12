d9486059d19f25e0e398077488a57246105fa649
Diego <diegopaulino_3@hotmail.com>
2023-07-12 03:30:02 -0300

    finishing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,15 +9,9 @@ import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PagesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 //Components
+import { RouterModule } from '@angular/router';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
@@ -9,6 +10,6 @@ import { PokeSearchComponent } from './poke-search/poke-search.component';
 @NgModule({
   declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
